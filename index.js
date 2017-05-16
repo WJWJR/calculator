a5d6b525537e51cea66fef1905c899ad11331e4e
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-05-16 15:12:21 -0400

    calculator works as requested

--- index.js
+++ index.js
@@ -48,21 +48,78 @@ let clearButton = document.querySelector('.clear')
 clearButton.addEventListener('click', handleClear)
 //then we will call a fuction that will clear the input.
 
+let addButton = document.querySelector('.add');
+let minusButton = document.querySelector('.minus');
+let multiplyButton = document.querySelector('.multiply')
+let divideButton = document.querySelector('.divide')
 
 
-let addButton = document.querySelector('.add')
+function useOperator(){
+  if (numberStorage.includes("+")) {
+  addProblem();
+  numberStorage =[]
+  console.log('it worked');
+  }
+  else if (numberStorage.includes("-")) {
+  minusProblem();
+  numberStorage =[]
+  console.log('it worked');
+}else if (numberStorage.includes("x")) {
+  multiplyProblem();
+
+  }else if (numberStorage.includes("÷")) {
+  divideProblem();
+  }
+  numberStorage=[]
+}
+
 
 addButton.addEventListener('click', handleNumberOrOperatorClick)
+minusButton.addEventListener('click', handleNumberOrOperatorClick)
+multiplyButton.addEventListener('click', handleNumberOrOperatorClick)
+divideButton.addEventListener('click', handleNumberOrOperatorClick)
 
-function solveProblem(){
-  let numbers = numberStorage.filter(item => item !== '+')
-  let solution = 0
-  for (var i = 0; i < numbers.length; i++) {
-    solution = solution + parseInt(numbers[i])
-  }
+
+function addProblem(){
+  let numbers = numberStorage.filter(item => item !== '+' )
+  let solution = parseInt(numbers[0]) + parseInt(numbers[1])
+  // for (var i = 0; i < numbers.length; i++) {
+  //   solution = solution + parseInt(numbers[i])
+  // }
   answerElement.innerText = solution
 }
 
+function minusProblem(){
+ let numbers = numberStorage.filter(item => item !== '-')
+  let solution = numbers[0] - numbers[1]
+  // for (var i = 0; i < numbers.length; i++) {
+  //   solution = solution - parseInt(numbers[i])
+  // }
+  answerElement.innerText = solution
+}
+
+function multiplyProblem(){
+  let numbers = numberStorage.filter(item => item !== 'x' )
+  // let numbers = numberStorage.filter(item => item !== '-')
+  console.log(numbers)
+  let solution = numbers[0] * numbers[1]
+  // for (var i = 0; i < numbers.length; i++) {
+  //   solution = solution * parseInt(numbers[i])
+  // }
+  answerElement.innerText = solution
+}
+
+function divideProblem(){
+  let numbers = numberStorage.filter(item => item !== '÷' )
+  let solution = numbers[0] / numbers[1]
+  // for (var i = 0; i < numbers.length; i++) {
+  //   solution = solution / parseInt(numbers[i])
+  // }
+  answerElement.innerText = solution
+}
+
+
+
 let equalsButton = document.querySelector('#equals')
 
-equalsButton.addEventListener("click" , solveProblem)
+equalsButton.addEventListener("click" , useOperator)
